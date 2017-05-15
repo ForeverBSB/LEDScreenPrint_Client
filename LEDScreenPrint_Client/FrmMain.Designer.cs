76b390afc9617fa4b6b5f8738a0072bb1a99d95a
绝世好博 <Fbsb@live.cn>
2017-05-15 11:03:15 +0800

    添加项目文件。

---
+++ LEDScreenPrint_Client/FrmMain.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace LEDScreenPrint_Client
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(37, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(436, 264);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(525, 51);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(436, 264);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox1";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(37, 347);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(906, 132);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox1";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Location = new System.Drawing.Point(37, 513);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(906, 132);
+            this.groupBox4.TabIndex = 0;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "groupBox1";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1314, 841);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.Opacity = 0.95D;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LED屏幕设置客户端";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+    }
+}
+
---
+++ LEDScreenPrint_Client/FrmMain.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LEDScreenPrint_Client
+{
+    public partial class FrmMain : Form
+    {
+        private String socketIPAddress;
+        private int socketPort;
+        private byte[] socketResult = new byte[1024];
+        private Socket socketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private String configFilePath;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+            //获取Ini配置文件绝对路径
+            configFilePath = System.Windows.Forms.Application.StartupPath + "\\Config.ini";
+            //获取Server服务IP地址
+            socketIPAddress = ConfigHelper.ReadIniData("Setting", "ServerIP", "127.0.0.1", configFilePath);
+            //获取Server服务端口号
+            socketPort = Convert.ToInt32(ConfigHelper.ReadIniData("Setting", "ServerPort", "8888", configFilePath));
+            //初始化连接
+            initSocket();
+        }
+
+        private void initSocket()
+        {
+            //设定服务器IP地址
+            IPAddress IP = IPAddress.Parse(socketIPAddress);
+            try
+            {
+                //配置服务器IP与端口
+                socketClient.Connect(new IPEndPoint(IP, socketPort));
+            }
+            catch
+            {
+                MessageBox.Show("连接服务器失败，请重新设置！");
+                Application.Exit();
+            }
+            Thread receiveThread = new Thread(ReceiveMessage);
+            receiveThread.Start();
+        }
+
+        private void ReceiveMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    //通过clientSocket接收数据
+                    int receiveNumber = socketClient.Receive(socketResult);
+                    string strContent = Encoding.ASCII.GetString(socketResult, 0, receiveNumber);
+                    Console.WriteLine("接收服务端{0}消息{1}", socketClient.RemoteEndPoint.ToString(), Encoding.ASCII.GetString(socketResult, 0, receiveNumber));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    socketClient.Shutdown(SocketShutdown.Both);
+                    socketClient.Close();
+                    break;
+                }
+            }
+        }
+
+        private void SendMessage(string message)
+        {
+            string sendMessage = message;
+            socketClient.Send(Encoding.ASCII.GetBytes(sendMessage));
+        }
+
+    }
+}
